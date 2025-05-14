4889010b3a32b842e164d5347727c9e469cb28f7
nagasai7772 <nagunagasai@gmail.com>
2025-05-14 13:31:55 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './Login'
-import SignUp from './SignUp'
+import SignUp from './Signup'
 import Home from './Home'
 // import YourMovies from './YourMovies'
 import Likes from './Liked'
